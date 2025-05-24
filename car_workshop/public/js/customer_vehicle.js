ceb95c752adc236b91e1ecb27ff028de4bcd3da8
dannyaudian <danny.a.pratama@gmail.com>
2025-05-24 19:27:06 +0700

    fix

--- car_workshop/public/js/customer_vehicle.js
+++ car_workshop/public/js/customer_vehicle.js
@@ -1,9 +1,8 @@
-frappe.ui.form.on('Customer Vehicle', {
+frappe.ui.form.on("Customer Vehicle", {
     refresh: function(frm) {
-        // Hanya tampilkan tombol jika dokumen sudah disimpan
+        // Tampilkan tombol jika dokumen sudah disimpan
         if (!frm.is_new()) {
             frm.add_custom_button(__('Lihat Perubahan Terakhir'), function() {
-                // Panggil API untuk mendapatkan log perubahan terakhir
                 frappe.call({
                     method: 'car_workshop.car_workshop.api.get_latest_vehicle_log',
                     args: {
@@ -11,27 +10,19 @@ frappe.ui.form.on('Customer Vehicle', {
                     },
                     callback: function(response) {
                         if (response.message) {
-                            // Jika log ditemukan
                             const log = response.message;
+                            const formattedDate = frappe.datetime.str_to_user(log.change_date);
                             
-                            // Format tanggal ke format lokal
-                            const formattedDate = frappe.datetime.str_to_user(
-                                log.change_date
-                            );
-                            
-                            // Siapkan pesan yang akan ditampilkan
                             let message = `
                                 <div style="max-width: 500px;">
                                     <div class="font-weight-bold mb-2 text-primary">
                                         ${log.change_type || 'Perubahan'}
                                     </div>
-                                    
                                     <div class="row mb-2">
                                         <div class="col-4 font-weight-bold">Field</div>
                                         <div class="col-8">${frappe.meta.get_label('Customer Vehicle', log.fieldname) || log.fieldname}</div>
                                     </div>`;
                             
-                            // Tambahkan old value dan new value jika tersedia
                             if (log.old_value || log.new_value) {
                                 message += `
                                     <div class="row mb-2">
@@ -43,8 +34,7 @@ frappe.ui.form.on('Customer Vehicle', {
                                         <div class="col-8">${log.new_value || '-'}</div>
                                     </div>`;
                             }
-                            
-                            // Tambahkan informasi tanggal dan user
+
                             message += `
                                 <div class="row mb-2">
                                     <div class="col-4 font-weight-bold">Tanggal</div>
@@ -55,7 +45,6 @@ frappe.ui.form.on('Customer Vehicle', {
                                     <div class="col-8">${log.updated_by}</div>
                                 </div>`;
                             
-                            // Tambahkan remarks jika tersedia
                             if (log.remarks) {
                                 message += `
                                     <div class="row mb-2">
@@ -63,17 +52,15 @@ frappe.ui.form.on('Customer Vehicle', {
                                         <div class="col-8">${log.remarks}</div>
                                     </div>`;
                             }
-                            
+
                             message += `</div>`;
-                            
-                            // Tampilkan pesan
+
                             frappe.msgprint({
                                 title: __('Perubahan Terakhir'),
                                 indicator: 'blue',
                                 message: message
                             });
                         } else {
-                            // Jika tidak ada log yang ditemukan
                             frappe.msgprint({
                                 title: __('Info'),
                                 indicator: 'gray',
@@ -83,14 +70,36 @@ frappe.ui.form.on('Customer Vehicle', {
                     }
                 });
             }, __('Actions'));
-            
-            // Tambahkan tombol untuk melihat semua perubahan
+
             frm.add_custom_button(__('Lihat Semua Perubahan'), function() {
                 frappe.route_options = {
                     customer_vehicle: frm.doc.name
                 };
                 frappe.set_route('List', 'Vehicle Change Log');
             }, __('Actions'));
+        }
+    },
+
+    brand: function(frm) {
+        frm.set_value("model", "");
+        frm.set_query("model", function() {
+            return {
+                filters: {
+                    brand: frm.doc.brand
+                }
+            };
+        });
+    },
+
+    onload: function(frm) {
+        if (frm.doc.brand) {
+            frm.set_query("model", function() {
+                return {
+                    filters: {
+                        brand: frm.doc.brand
+                    }
+                };
+            });
         }
     }
 });
